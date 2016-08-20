7e8c128775b757f3d3c5a0900ab553ec25a0d2ca
Lazarev Mikhail <lazarevm93@gmail.com>
2016-08-20 22:00:43 +0500

    Реализована команда /help

--- bot/commands/help.py
+++ bot/commands/help.py
@@ -3,4 +3,10 @@ from bot.commands.abstract_command import AbstractCommand
 
 class HelpCommand(AbstractCommand):
     def _execute(self, db, user, bot, update):
-        bot.send_message(chat_id=update.message.chat_id, text='HelpCommand')
+        help_template = 'Список команд: \n' \
+                        '/shows - вывести список всех сериалов \n' \
+                        '/subscribe - подписаться на сериал \n' \
+                        '/unsubscribe - отписаться от сериала \n' \
+                        '/unsubscribe_all - отписаться от всех сериалов \n' \
+                        '/help - справка'
+        bot.send_message(chat_id=update.message.chat_id, text=help_template)
