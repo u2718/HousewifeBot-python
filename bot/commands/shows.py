6a762f3b34086bd66be3589d0593373fea316335
Lazarev Mikhail <lazarevm93@gmail.com>
2016-08-20 21:36:10 +0500

    Реализована команда /shows

--- bot/commands/shows.py
+++ bot/commands/shows.py
@@ -1,6 +1,10 @@
 from bot.commands.abstract_command import AbstractCommand
+from data.database import Show
 
 
 class ShowsCommand(AbstractCommand):
     def _execute(self, db, user, bot, update):
-        bot.send_message(chat_id=update.message.chat_id, text='ShowsCommand')
+        shows = ['%s (%s)' % (show.title, show.original_title) for show in db.query(Show)]
+        for shows_part in (shows[:len(shows)//2], shows[len(shows)//2:]):
+            result = '\n'.join(shows_part)
+            bot.send_message(chat_id=update.message.chat_id, text=result)
