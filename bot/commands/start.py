eaf3d3ce4c420e39f9ee71d45deebc68fbf36987
Lazarev Mikhail <lazarevm93@gmail.com>
2016-08-20 21:59:21 +0500

    Реализована команда /start

--- bot/commands/start.py
+++ bot/commands/start.py
@@ -3,4 +3,11 @@ from bot.commands.abstract_command import AbstractCommand
 
 class StartCommand(AbstractCommand):
     def _execute(self, db, user, bot, update):
-        bot.send_message(chat_id=update.message.chat_id, text='StartCommand')
+        help_template = 'Шалом, %s. \n' \
+                        'Список команд: \n' \
+                        '/shows - вывести список всех сериалов \n' \
+                        '/subscribe - подписаться на сериал \n' \
+                        '/unsubscribe - отписаться от сериала \n' \
+                        '/unsubscribe_all - отписаться от всех сериалов \n' \
+                        '/help - справка'
+        bot.send_message(chat_id=update.message.chat_id, text=help_template % update.message.from_user.first_name)
