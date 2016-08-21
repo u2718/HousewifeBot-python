582d68acce3e23d2521106fffecfa7496e467241
Lazarev Mikhail <lazarevm93@gmail.com>
2016-08-21 22:37:43 +0500

    Обработка неизвестных команд

--- bot/commands/unknown.py
+++ bot/commands/unknown.py
@@ -3,4 +3,4 @@ from bot.commands.abstract_command import AbstractCommand
 
 class UnknownCommand(AbstractCommand):
     def _execute(self, db, user, bot, update, args):
-        bot.send_message(chat_id=update.message.chat_id, text='UnknownCommand')
+        bot.send_message(chat_id=update.message.chat_id, text='Пощади, братишка')
