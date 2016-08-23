9f0fef1d6a79a4cbe820ff56141710d1abc438a9
Lazarev Mikhail <lazarevm93@gmail.com>
2016-08-23 21:42:13 +0500

    Реализована команда /unsubscribe_all

--- bot/bot.py
+++ bot/bot.py
@@ -1,11 +1,12 @@
 import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, CallbackQueryHandler
 from bot.commands.help import HelpCommand
 from bot.commands.shows import ShowsCommand
 from bot.commands.start import StartCommand
 from bot.commands.subcribe import SubscribeCommand
 from bot.commands.unknown import UnknownCommand
 from bot.commands.unsubscribe import UnsubscribeCommand
+from bot.commands.unsubscribe_all import UnsubscribeAllCommand
 from utils.config import Config
 
 
@@ -14,10 +15,16 @@ class Bot:
         self.bot = telegram.Bot(token=Config().token)
         self.updater = Updater(token=Config().token)
         self.dispatcher = self.updater.dispatcher
+
         shows_handler = CommandHandler('shows', ShowsCommand().execute)
+        self.dispatcher.add_handler(shows_handler)
+
         start_handler = CommandHandler('start', StartCommand().execute)
+        self.dispatcher.add_handler(start_handler)
+
         help_handler = CommandHandler('help', HelpCommand().execute)
-        unknown_handler = MessageHandler([], UnknownCommand().execute)
+        self.dispatcher.add_handler(help_handler)
+
         subscribe_command = SubscribeCommand()
         subscribe_handler = ConversationHandler(
             entry_points=[CommandHandler('subscribe', subscribe_command.execute, pass_args=True)],
@@ -26,6 +33,8 @@ class Bot:
             },
             fallbacks=[]
         )
+        self.dispatcher.add_handler(subscribe_handler)
+
         unsubscribe_command = UnsubscribeCommand()
         unsubscribe_handler = ConversationHandler(
             entry_points=[CommandHandler('unsubscribe', unsubscribe_command.execute, pass_args=True)],
@@ -34,12 +43,14 @@ class Bot:
             },
             fallbacks=[]
         )
-
-        self.dispatcher.add_handler(shows_handler)
-        self.dispatcher.add_handler(start_handler)
-        self.dispatcher.add_handler(help_handler)
-        self.dispatcher.add_handler(subscribe_handler)
         self.dispatcher.add_handler(unsubscribe_handler)
+
+        unsubscribe_all_command = UnsubscribeAllCommand()
+        unsubscribe_all_handler = CommandHandler('unsubscribe_all', unsubscribe_all_command.execute)
+        self.dispatcher.add_handler(CallbackQueryHandler(unsubscribe_all_command.unsubscribe_all))
+        self.dispatcher.add_handler(unsubscribe_all_handler)
+
+        unknown_handler = MessageHandler([], UnknownCommand().execute)
         self.dispatcher.add_handler(unknown_handler)
 
     def start_polling(self):
--- bot/commands/unsubscribe_all.py
+++ bot/commands/unsubscribe_all.py
@@ -1,6 +1,31 @@
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from bot.commands.abstract_command import AbstractCommand
+from data import database
+from data.database import Subscription
 
 
 class UnsubscribeAllCommand(AbstractCommand):
     def _execute(self, db, user, bot, update, args):
-        bot.send_message(chat_id=update.message.chat_id, text='UnsubscribeAllCommand')
+        self.db = db
+        self.user = user
+        if not user.subscriptions:
+            bot.send_message(chat_id=update.message.chat_id, text='Вы не подписаны ни на один сериал')
+            return
+
+        keyboard = [[InlineKeyboardButton("Да", callback_data='yes'), InlineKeyboardButton("Нет", callback_data='no')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        bot.sendMessage(update.message.chat_id, text="Вы действительно хотите отписаться от всех сериалов?",
+                        reply_markup=reply_markup)
+
+    def __handled(self):
+        database.close(self.db)
+
+    def _handled(self, db):
+        pass
+
+    def unsubscribe_all(self, bot, update):
+        if update.callback_query.data == 'yes':
+            for s in self.user.subscriptions:
+                self.db.query(Subscription).filter(Subscription.id == s.id).delete()
+            bot.send_message(chat_id=update.callback_query.message.chat.id, text='Вы отписались от всех сериалов')
+        self.__handled()
